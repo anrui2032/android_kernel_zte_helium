f33aac9a30b2bf09d16ea0141999176a4a3f9499
anrui2032 <anrui2032@gmail.com>
2025-09-16 19:47:17 +0800

    gpu: Import ZTE changes
    Change-Id: I0a57e4b3830263d925d2ab77504c94dbacc50224

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5063,6 +5063,9 @@ static void _unregister_device(struct kgsl_device *device)
 	mutex_unlock(&kgsl_driver.devlock);
 }
 
+#ifdef CONFIG_ZTE_LCD_HBM_CTRL
+extern struct kgsl_device *ksgl_uevent_device;
+#endif
 static int _register_device(struct kgsl_device *device)
 {
 	static u64 dma_mask = DMA_BIT_MASK(64);
@@ -5101,6 +5104,9 @@ static int _register_device(struct kgsl_device *device)
 		return ret;
 	}
 
+#ifdef CONFIG_ZTE_LCD_HBM_CTRL
+	ksgl_uevent_device = device;
+#endif
 	device->dev->dma_mask = &dma_mask;
 	arch_setup_dma_ops(device->dev, 0, 0, NULL, false);
 
